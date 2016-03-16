2040c80a3fbd17a8ad8540e124016ff92f2b29a0
Shaun Hallier <shaunhallier@gmail.com>
2016-03-16 02:02:34 -0700

    added SETTINGS.py, but its not being used yet

---
+++ SETTINGS.py
@@ -0,0 +1,12 @@
+APP_KEY = 'XXXxxXXXXXxxXXXXxXxxxXxxXxXXxxXxXxXxXXXxX' 
+APP_SECRET = 'xxxXXXXXxXxXxxxXXxXxXxxXXXXxxXxXXXxxXXxxXxxxxxX' 
+OAUTH_TOKEN = 'XXxXXXXXxxXXxxXxXxxxXXXxXxxxXxXXXxxXxXxXxxXxxx'
+OAUTH_TOKEN_SECRET = 'XXXxXXXxXxxXXXxXxxXxXXxXXXxXxXXXXxXxXXxXX'
+
+## Your Telegram Channel Name ##
+channel_name = 'geremysays'
+## Telegram Access Token ##
+telegram_token = 'nnnnnnnnnnnnn:xxxXXXxXxXxXxXXxXxxXxxxxXx'
+## Twitter User Name to get Timeline ##
+user_name = 'GeremySays'
+
