b027b711a6479ee874b706a2524bd6e6abd687b8
danny <65585589+ziac007@users.noreply.github.com>
2020-06-19 15:07:17 +0530

    Update twitterSays.py

--- twitterSays.py
+++ twitterSays.py
@@ -1,7 +1,7 @@
 import os, time
-import cPickle as pickle
+import pickle as pickle
 import twython as Twython
-from urllib import quote
+from urllib.parse import quote
 from SETTINGS import *
 
 api = Twython.Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
@@ -38,9 +38,9 @@ def main():
     user_timeline = get_timeline(latest_tweet_id)
     number_of_tweets = len(user_timeline)
     if number_of_tweets > 0:
-        for i in reversed(range(0,number_of_tweets)):
+        for i in reversed(list(range(0,number_of_tweets))):
             if user_timeline[i]['text']:
-                print user_timeline[i]['text']
+                print(user_timeline[i]['text'])
                 send_message(user_timeline[i]['text'])
                 time.sleep(4)
         latest_tweet_id = user_timeline[0]['id']
